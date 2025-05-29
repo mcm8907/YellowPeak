7bd439e5767653aa5e8290254128275d614a9d6d
mcm8907 <mcm8907@nyu.edu>
2025-05-29 14:40:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { createClient } = require("@supabase/supabase-js");
-require("dotenv\config");
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
